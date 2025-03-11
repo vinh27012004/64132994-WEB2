a239ac7b0540fc69a4e16376b41e5516a917e373
Pham Nguyen Tri Vinh <phamvinh324@gmail.com>
2025-03-11 08:59:36 +0700

    push model

--- Bai2-SpringBoot/src/main/java/ntu/vinh/models/Student.java
+++ Bai2-SpringBoot/src/main/java/ntu/vinh/models/Student.java
@@ -1,5 +1,31 @@
 package ntu.vinh.models;
 
 public class Student {
+    private String mssv;
+    private String hoTen;
+    private int namSinh;
+    private String gioiTinh;
 
+    public Student(String mssv, String hoTen, int namSinh, String gioiTinh) {
+        this.mssv = mssv;
+        this.hoTen = hoTen;
+        this.namSinh = namSinh;
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getMssv() {
+        return mssv;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public int getNamSinh() {
+        return namSinh;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
 }
