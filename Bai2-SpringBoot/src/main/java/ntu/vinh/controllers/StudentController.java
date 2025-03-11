03fc4d17d242ab0a6df2a276608ccbd3b596a71c
Pham Nguyen Tri Vinh <phamvinh324@gmail.com>
2025-03-11 08:58:40 +0700

    push

--- Bai2-SpringBoot/src/main/java/ntu/vinh/controllers/StudentController.java
+++ Bai2-SpringBoot/src/main/java/ntu/vinh/controllers/StudentController.java
@@ -1,5 +1,21 @@
 package ntu.vinh.controllers;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ntu.vinh.models.Student;
+
+@Controllers
 public class StudentController {
+	@GetMapping("/student")
+    public String studentInfo(Model model) {
+        // Tạo đối tượng sinh viên
+        Student student = new Student("SV001", "Nguyễn Văn A", 2002, "Nam");
+
+        // Đưa dữ liệu vào Model để truyền sang View
+        model.addAttribute("student", student);
+
+        return "student"; // Trả về trang student.html
+    }
 
 }
