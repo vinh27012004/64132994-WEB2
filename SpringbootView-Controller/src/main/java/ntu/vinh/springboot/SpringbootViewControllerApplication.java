1de614019ae0bb1375ee76c986b67c0bc2b6618f
Pham Nguyen Tri Vinh <phamvinh324@gmail.com>
2025-03-18 10:03:54 +0700

    LoginSpringBoot(view-controller)

---
+++ SpringbootView-Controller/src/main/java/ntu/vinh/springboot/SpringbootViewControllerApplication.java
@@ -0,0 +1,13 @@
+package ntu.vinh.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootViewControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootViewControllerApplication.class, args);
+	}
+
+}
---
+++ SpringbootView-Controller/src/main/java/ntu/vinh/springboot/controllers/LoginController.java
@@ -0,0 +1,34 @@
+package ntu.vinh.springboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@Controller
+public class LoginController {	
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login"; // This will return the login.html template
+    }
+
+    @PostMapping("/login")
+    public String handleLogin(
+            @RequestParam String username,
+            @RequestParam String password,
+            Model model) {
+
+        // Simple authentication logic (for demonstration purposes)
+        if ("admin".equals(username) && "admin".equals(password)) {
+            model.addAttribute("message", "Đăng nhập thành công");
+        } else {
+            model.addAttribute("message", "Lỗi unsername hoặc password");
+        }
+
+        return "login";
+    }
+}
