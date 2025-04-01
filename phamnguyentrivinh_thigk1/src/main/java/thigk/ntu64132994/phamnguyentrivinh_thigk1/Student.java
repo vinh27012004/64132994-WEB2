eab0d27f9446d0a4035956417e420bb23210ff61
Pham Nguyen Tri Vinh <phamvinh324@gmail.com>
2025-04-01 09:24:11 +0700

    uplan2

---
+++ phamnguyentrivinh_thigk1/src/main/java/thigk/ntu64132994/phamnguyentrivinh_thigk1/Student.java
@@ -0,0 +1,38 @@
+package thigk.ntu64132994.phamnguyentrivinh_thigk1;
+
+public class Student {
+    private int id;
+    private String name;
+    private int groupId;
+
+    public Student(int id, String name, int groupId) {
+        this.id = id;
+        this.name = name;
+        this.groupId = groupId;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+}
---
+++ phamnguyentrivinh_thigk1/src/main/java/thigk/ntu64132994/phamnguyentrivinh_thigk1/Topic.java
@@ -0,0 +1,58 @@
+package thigk.ntu64132994.phamnguyentrivinh_thigk1;
+
+public class Topic {
+    private int id;
+    private String topicName;
+    private String topicDescription;
+    private int supervisorId;
+    private String topicType;
+
+    public Topic(int id, String topicName, String topicDescription, int supervisorId, String topicType) {
+        this.id = id;
+        this.topicName = topicName;
+        this.topicDescription = topicDescription;
+        this.supervisorId = supervisorId;
+        this.topicType = topicType;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
+
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
+    }
+
+    public String getTopicDescription() {
+        return topicDescription;
+    }
+
+    public void setTopicDescription(String topicDescription) {
+        this.topicDescription = topicDescription;
+    }
+
+    public int getSupervisorId() {
+        return supervisorId;
+    }
+
+    public void setSupervisorId(int supervisorId) {
+        this.supervisorId = supervisorId;
+    }
+
+    public String getTopicType() {
+        return topicType;
+    }
+
+    public void setTopicType(String topicType) {
+        this.topicType = topicType;
+    }
+}
