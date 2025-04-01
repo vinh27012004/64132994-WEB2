1d5145e73c8e50ae93a560059953abef91de9acb
Pham Nguyen Tri Vinh <phamvinh324@gmail.com>
2025-04-01 09:08:11 +0700

    uplan1

---
+++ phamnguyentrivinh_thigk1/src/main/java/thigk/ntu64132994/phamnguyentrivinh_thigk1/PhamnguyentrivinhThigk1Application.java
@@ -0,0 +1,13 @@
+package thigk.ntu64132994.phamnguyentrivinh_thigk1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamnguyentrivinhThigk1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamnguyentrivinhThigk1Application.class, args);
+	}
+
+}
---
+++ phamnguyentrivinh_thigk1/src/test/java/thigk/ntu64132994/phamnguyentrivinh_thigk1/PhamnguyentrivinhThigk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk.ntu64132994.phamnguyentrivinh_thigk1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamnguyentrivinhThigk1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
