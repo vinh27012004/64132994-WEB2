aae18459d3608f5b119129a8356b09ba07830b97
Pham Nguyen Tri Vinh <phamvinh324@gmail.com>
2025-04-19 21:47:30 +0700

    renew

---
+++ phamnguyentrivinh_thigk1_renew/src/test/java/thigk/ntu64132994/phamnguyentrivinh_thigk1_renew/PhamnguyentrivinhThigk1RenewApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk.ntu64132994.phamnguyentrivinh_thigk1_renew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamnguyentrivinhThigk1RenewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
